051431548be2f2a6564a9c68930cdf86bc443357
Prinsatrebus <73902088+Prinsatrebus@users.noreply.github.com>
2021-11-03 15:32:13 -0600

    Add files via upload
    Starter file

---
+++ tic-tac-toe.py
@@ -0,0 +1,12 @@
+# Placeholder variables
+
+Player1 = ' '
+Player2 = ' '
+row1 = ['','','']
+row2 = ['','','']
+row1 = ['','','']
+
+
+# Build out game board
+
+def display_board(row1, separator, row2, separator, row3):
